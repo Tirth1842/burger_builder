b9da260e754333c310bcd2855a58efc9e2eb8158
Tirth Patel <tirthpatel.1842000@gmail.com>
2021-07-29 10:27:56 +0530

    toolbar with css

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -18,7 +18,7 @@ const orderSummary = (props) =>  {
             <ul>
                 {ingredientSummary}
             </ul>
-            
+            <p><strong>TotalPrice: {props.TotalPrice.toFixed(2)}</strong></p>
             <p>Continue to checkout?</p>
             <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
             <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
