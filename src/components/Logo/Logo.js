e34f1a077ea20f34a5ea47b015729d63ff10372c
Tirth Patel <tirthpatel.1842000@gmail.com>
2021-08-03 10:34:31 +0530

    sidedrawer, navigation

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,9 +2,11 @@ import React from 'react';
 import burgerLogo from '../../assets/Images/28.1 burger-logo.png';
 import classes from './Logo.module.css';
 const logo = (props) => (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height: props.height}}> 
         <img src={burgerLogo} alt="MyBurger" />
     </div>
 );
 
 export default logo;
+
+// passing the css property as props.
