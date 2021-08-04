c57b1a194e084eb5327691b0b6338990b8e21ef3
Tirth Patel <tirthpatel.1842000@gmail.com>
2021-08-04 10:24:31 +0530

    improvement in order summary for unneccessary rend

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,40 @@
-import React from 'react';
+import React,  { Component } from 'react';
 
 import Aux from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
-const orderSummary = (props) =>  {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    // This could be a functional component, doesnt have to be a class component.
+
+    componentWillUpdate() {
+        console.log('[OrderSummary] WillUpdate');
+    }
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
             <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
             </li>);
         })
-
-    return (
-        <Aux>
+        return (
+            <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>TotalPrice: {props.TotalPrice.toFixed(2)}</strong></p>
+            <p><strong>TotalPrice: {this.props.TotalPrice.toFixed(2)}</strong></p>
             <p>Continue to checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Aux>
-    )
+        )
+    }
+    
+    // conversion to statefull component is because to add life cycle hook .
+// to check the unneccesary rendering of order summary.
+
+   
 };
 
-export default orderSummary;
+export default OrderSummary;
